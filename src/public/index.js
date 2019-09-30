ffc08602a11bb5b7c4f29fe5230040b6366c1349
Mike Jagd Winum <skupi1234@gmail.com>
2019-09-30 02:59:02 +0200

    added navbar and smooth scrolling between 'pages'

--- src/public/index.js
+++ src/public/index.js
@@ -1,6 +1,6 @@
-const remote = require('electron').remote;
+const remote = require('electron').remote;  //used to close and minimize the window
 
-let maximized = false;
+let page = 0;   //Used to prevent btn-spam, which causes the app the be stuck in the middle
 
 //Check if any of the menu-buttons have been clicked
 document.getElementById('close-btn').addEventListener('click',function(e){
@@ -10,4 +10,35 @@ document.getElementById('close-btn').addEventListener('click',function(e){
 document.getElementById('min-btn').addEventListener('click', function(e){
     let win = remote.getCurrentWindow();
     win.minimize();
+});
+
+//Check if any of the navbar-buttons have been clicked
+document.getElementById('home-btn').addEventListener('click', function(e){
+    if(page != 0){
+        let elem = document.getElementById('home');
+        let offset = 25;
+        let elementPos = elem.getBoundingClientRect().top;
+        let offsetPos = elementPos - offset;
+        
+        window.scrollTo({
+            top: offsetPos,
+            behavior: "smooth"
+        });
+        page = 0;
+    }
+    
+});
+document.getElementById('commands-btn').addEventListener('click', function(e){
+    if(page != 1){
+        let elem = document.getElementById('commands');
+        let offset = 25;
+        let elementPos = elem.getBoundingClientRect().top;
+        let offsetPos = elementPos - offset;
+        
+        window.scrollTo({
+            top: offsetPos,
+            behavior: "smooth"
+        });
+        page = 1;
+    }
 });
